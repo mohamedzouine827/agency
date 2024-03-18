ea078299607f2c042d146657fb2a2f75892404d6
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-18 15:08:02 +0000

    8

--- pages/components/Services/ServicesSection.tsx
+++ pages/components/Services/ServicesSection.tsx
@@ -5,20 +5,20 @@ export default function ServicesSection() {
         <div className="mx-[6vw] mt-[12vh] border bg-black h-[20vh] flex flex-col justify-center items-center gap-[6vh]">
             <div className="flex flex-row gap-[18vh]">
                 <div className="flex flex-row justify-center items-center gap-[1vh]">
-                    <SlSocialGoogle color="white" className="h-[5.2vh]"/>
-                    <span className="text-white text-[2.8vh]">Google</span>
+                    <SlSocialGoogle color="white" className="h-[6vh] w-[2vw]"/>
+                    <span className="text-white text-[3vh]">Google</span>
                 </div>
                 <div className="flex flex-row justify-center items-center gap-[1vh]">
-                    <SlSocialYoutube color="white" className="h-[5.2vh]"/>
-                    <span className="text-white text-[2.8vh]">Youtube</span>
+                    <SlSocialYoutube color="white" className="h-[6vh] w-[2vw]"/>
+                    <span className="text-white text-[3vh]">Youtube</span>
                 </div>
                 <div className="flex flex-row justify-center items-center gap-[1vh]">
-                    <SlSocialGithub color="white" className="h-[5.2vh]"/>
-                    <span className="text-white text-[2.8vh]">Github</span>
+                    <SlSocialGithub color="white" className="h-[6vh] w-[2vw]"/>
+                    <span className="text-white text-[3vh]">Github</span>
                 </div>
                 <div className="flex flex-row justify-center items-center gap-[1vh]">
-                    <SlSocialLinkedin color="white" className="h-[5.2vh]"/>
-                    <span className="text-white text-[2.8vh]">LinkedIn</span>
+                    <SlSocialLinkedin color="white" className="h-[6vh] w-[2vw]"/>
+                    <span className="text-white text-[3vh]">LinkedIn</span>
                 </div>
             </div>
         </div>
