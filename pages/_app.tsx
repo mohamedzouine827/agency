14c8d0985b67050a3a3e06acd0ccf80777863ce1
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-19 02:15:42 +0000

    8

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function ZnAgency({ Component, pageProps }: AppProps) {
+  return (
+      <div>
+        <Head>
+          <title>ZnAgency</title>
+        </Head>
+        <Component {...pageProps} />
+      </div>
+  );
 }
+
+export default ZnAgency;
